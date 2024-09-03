db99f164e21a5837bc696dedab67a3c527444452
Andrew Jiang <andrew@buildwithfern.com>
2024-09-03 14:13:12 -0400

    switch to middleware routing

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const headers = new Headers(request.headers);
+  headers.set("X-Fern-Host", "octo.ai");
+
+  const url = request.nextUrl.clone();
+  url.host = "app-slash.ferndocs.com";
+
+  return NextResponse.rewrite(url, { request: { headers } });
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/docs/", "/docs/:path*/"],
+};
