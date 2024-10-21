ee283fd9f1148c7a2300fb03bdb6738999f449b7
Andrew Jiang <andrew@buildwithfern.com>
2024-10-21 13:02:05 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
   headers.set("X-Fern-Host", "buildwithfern.com");
 
   const url = request.nextUrl.clone();
-  url.host = "fern-middleware-rewrite-test.vercel.app";
+  url.host = "app.ferndocs.com";
 
   return NextResponse.rewrite(url, { request: { headers } });
 }
